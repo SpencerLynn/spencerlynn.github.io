1e4653d5b307f64112834d849f76d0cee1d77728
Spencer Lynn <sllynn8907@gmail.com>
2023-09-21 15:04:35 -0500

    Small tweaks to about page including adding a photo

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,6 +4,8 @@ import styled from 'styled-components';
 import { Content, Layout } from 'components';
 import { media } from '../utils/media';
 
+import profilePic from '../assets/profile-pic.jpg';
+
 const Paragraph = styled.p`
   font-size: 1.2rem;
   margin-top: -0.5rem;
@@ -15,17 +17,25 @@ const Paragraph = styled.p`
   }
 `;
 
-const Title = styled.h1`
-  font-size: 2.3rem;
-  margin-bottom: 1.25rem;
-`;
+const Image = styled.img`
+  max-width: 200px;
+  border-radius: 12px;
+  box-shadow: 3px 3px 10px #00000077;
+  display: block;
+  margin-right: auto;
+  margin-left: auto;
+`
 
 const About = () => (
   <Layout>
     <Content>
-      <Title>Hi.</Title>
+      <Image
+        src={profilePic}
+        alt={'Profile picture'}
+        draggable={false}
+      />
       <Paragraph>
-        My name is Spencer Lynn. I am a software engineer located in Kirkland, Washington. I am originally from Birmingham, Alabama. I graduated Auburn University with a degree in Wireless Software Engineering. War Eagle!
+        Hi, I'm Spencer Lynn. I am a software engineer working on mobile and web apps. I am currently located in Auburn, AL by way of Birmingham, AL, Huntsville, AL, and the Greater Seattle Area. I graduated Auburn University with a degree in Wireless Software Engineering. War Eagle!
         I am the master of getting computers to do things, occasionally cool things. Well, except for when it doesn't work, which is more often than I'd like.
       </Paragraph>
     </Content>
